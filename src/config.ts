9b446e26956906c8120c62a50c0e5f34150061eb
SotaSuzuki <3.3sota.suzuki@gmail.com>
2024-01-15 16:51:41 +0900

    configure deployment setting for github pages

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
+  website: "https://sotszk.github.io",
+  author: "",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
   title: "AstroPaper",
   ogImage: "astropaper-og.jpg",
